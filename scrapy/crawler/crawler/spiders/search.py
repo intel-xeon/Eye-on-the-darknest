03c564895cc1356e3a3e0027bd2e3aed0c050637
intel-xeon <37773731+intel-xeon@users.noreply.github.com>
2022-06-20 17:25:51 +0200

    Update search.py

--- scrapy/crawler/crawler/spiders/search.py
+++ scrapy/crawler/crawler/spiders/search.py
@@ -1,16 +1,13 @@
 import scrapy
 from scrapy.spiders import  Rule
 from scrapy.linkextractors import LinkExtractor
-import time
+from time import sleep
 import urllib.parse
-import getopt
-import sys
+import unicodedata
 from scrapy.exporters import JsonItemExporter
+import html
 
 
-
-escape_char_double = "ESCAPING_CHARACTER_FOR_DOUBLE_QUOTES"
-escape_char_one = "ESCAPING_CHARACTER_FOR_ONE_QUOTES"
 
 def testmultiple(q,stringa):
     app = 0
@@ -20,15 +17,12 @@ def testmultiple(q,stringa):
             stringa = stringa[app:]
         else:
             return False
-    #print("Sto per dare true qui q:"+q+" stringa:"+stringa)
     return True
 
 def writereport(lista):
     w = "["
     for x in lista:
         k = str(x).replace("'",'"')
-        k = k.replace(escape_char_one,"'")
-        k = k.replace(escape_char_double,'\\"')
         w+=k
         w+=","
     w = w[0:len(w)-1]
@@ -40,7 +34,6 @@ def writereport(lista):
 
 
 def searchforstring(response,string,splitchar):
-    #element = ["div","a","p","td","li","ul","small"]
     element = ['a','abbr','acronym','address','applet','area','article','aside','audio','b','base','basefont','bdi','bdo','bgsound','big','blink','blockquote','body','br','button','canvas','caption','center','circle','cite','clipPath','code','col','colgroup','command','content','data','datalist','dd','defs','del','details','dfn','dialog','dir','div','dl','dt','element','ellipse','em','embed','fieldset','figcaption','figure','font','footer','foreignObject','form','frame','frameset','g','h1','h2','h3','h4','h5','h6','head','header','hgroup','hr','html','i','iframe','image','img','input','ins','isindex','kbd','keygen','label','legend','li','line','linearGradient','link','listing','main','map','mark','marquee','mask','math','menu','menuitem','meta','meter','multicol','nav','nextid','nobr','noembed','noframes','noscript','object','ol','optgroup','option','output','p','param','path','pattern','picture','plaintext','polygon','polyline','pre','progress','q','radialGradient','rb','rbc','rect','rp','rt','rtc','ruby','s','samp','script','section','select','shadow','slot','small','source','spacer','span','stop','strike','strong','style','sub','summary','sup','svg','table','tbody','td','template','text','textarea','tfoot','th','thead','time','title','tr','track','tspan','tt','u','ul','var','video','wbr','xmp']
     k = []      
     list_string = string.split(splitchar)
@@ -54,15 +47,13 @@ def searchforstring(response,string,splitchar):
                 for x in response.xpath('//'+e+'/text()').extract():
                     if("*" in j):
                         if(testmultiple(j.lower(),x.lower())):
-                            x=x.replace("'",escape_char_one)
-                            x=x.replace('"',escape_char_double)
-                            x = x.replace("\xa0",' ') #Rimozione carattere sporco \xa0
+                            x = unicodedata.normalize("NFKD", x) # serve per pulire carattere sporco \xa0
+                            x = html.escape(x)
                             match.append(x)
                     else:
                         if(j.lower() in x.lower()):
-                            x=x.replace("'",escape_char_one)
-                            x=x.replace('"',escape_char_double)
-                            x = x.replace("\xa0",' ') #Rimozione carattere sporco \xa0
+                            x = html.escape(x)
+                            x = unicodedata.normalize("NFKD", x) # serve per pulire carattere sporco \xa0
                             match.append(x)
             except Exception:
                 continue
@@ -80,7 +71,6 @@ def getHost(url):
 
 
 def extractLink(url,link):
-    #print(link)
     domain = url.split("/")[0]+'//'
     domain += getHost(url)
     try:
@@ -90,26 +80,16 @@ def extractLink(url,link):
     app = link[i:]
     i = app.index('=')+2
     app = app[i:]
-    ##print("APP ADESSO "+app)
-    ##time.sleep(2)
     x = app.index("\"")
     uri = app[0:x]
-    ##print("URI:"+uri)
-    ##print("URI "+uri+"\nLINK: "+link)
     if(len(uri)==0):
         return
     if(uri[0]!='/'):
         domain+='/'
     if (len(getHost(uri))==0):
-        ##print("Ritorno: "+domain+uri)
         return domain+uri
     else:
-        ##print("Ritorno "+uri)
         return uri
-
-
-
-
 
 class searchSpider(scrapy.Spider):
     
@@ -118,12 +98,6 @@ class searchSpider(scrapy.Spider):
     string = ""
     def __init__(self, *args, **kwargs):
         super(searchSpider, self).__init__(*args, **kwargs)
-
-            
-    
-    #splitchar = input("Inserisci un separatore di stringhe: ")
-    #string = input("Inserisci la stringa da ricercare (per separare più stringhe utilizzare il separatore '"+splitchar+"' ): ")
-    #time.sleep(10)
     name = "search"
     le1 = LinkExtractor()
     rules = (Rule(le1, callback='parse_item'))
@@ -146,19 +120,12 @@ class searchSpider(scrapy.Spider):
                 urls.append(x[0:len(x)-1])
             else:
                 urls.append(x)
-     #  urls = ["https://quotes.toscrape.com/page/1/","https://quotes.toscrape.com/page/2/"]
         for url in urls:
             self.root = getHost(url)
-            #print("Metodo principale root: "+self.root)
-            #time.sleep(5)
             sw = True
             yield scrapy.Request(url=url, callback=self.parse,cb_kwargs=dict(radix=self.root,switch=sw,list_json=self.list_json))
 
     def parse(self, response,radix,switch,list_json):
-        #print(switch)
-        #print("Mi è arrivato: "+radix)
-        #print(response.url)
-        #time.sleep(7)
         try:
             self.onlyscope
         except Exception:
@@ -172,7 +139,6 @@ class searchSpider(scrapy.Spider):
                         continue
                 print(k.url)
                 u.append(k.url)
-            #time.sleep(2)
             links = response.xpath("//a").extract()
             for a in links:
                 ur = extractLink(response.url,a)
@@ -183,29 +149,19 @@ class searchSpider(scrapy.Spider):
                     print(ur)
                     if (ur not in u and len(ur)>0):
                         u.append(ur)
-            #time.sleep(2)
-           # print("\n\n\n\n\n\n")
             for x in u:
                 print(x)
-            #print("\n\n\n\n\n\n")
-            #time.sleep(9)
             for proc in u:
                 if (radix in getHost(proc)):
                     switch = True
-                    #print("root ("+radix+") è in proc ("+proc+") quindi setto sw su True ")
-                    ##time.sleep(1)
                 else:
                     switch = False
-                    #print("root ("+radix+") NON è in proc ("+proc+") quindi setto sw su False ")
-                    #time.sleep(1)
                 yield scrapy.Request(url=proc, callback=self.parse,cb_kwargs=dict(radix=radix,switch=switch,list_json=list_json))
         
         matched = searchforstring(response,self.string,self.splitchar)
         if len(matched)>0:
             list_json.append({'url':response.url,'title': response.css('title::text').get(),'data':matched})
             writereport(list_json)
-
-        #    yield {'url':response.url,'title': response.css('title::text').get(),'match':matched}
         
 
     
