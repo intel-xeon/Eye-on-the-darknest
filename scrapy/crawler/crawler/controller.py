7a93cb8c3d321b745901044dd93616a05cde63bb
intel-xeon <37773731+intel-xeon@users.noreply.github.com>
2022-06-29 11:20:52 +0200

    Update controller.py

--- scrapy/crawler/crawler/controller.py
+++ scrapy/crawler/crawler/controller.py
@@ -4,6 +4,9 @@ import os
 import socket
 import time
 import shutil
+import random
+
+#Da implementare il parametro regex nello spider
 
 def torEnabled():
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -20,19 +23,82 @@ def initpath(path):
     f = open(path+"result.json","w")
     f.write("{\"data\":[]}")
     f.close()
-    
 
+
+
+        
+def isvalidregexfieldsingle(maximum,regex,error):
+    if(not regex.isdigit()):
+        error.append("Only integer number is accepted. Error on '"+regex+"'")
+        return error
+    elif(int(regex)>maximum):
+        error.append("Index out of rangemax accepted:"+str(maximum)+" your value:"+regex+"")
+        return error
+    return error
+
+def isvalidregexfield(maximum,splitter,string,regex,error):
+    if(len(splitter)==0):
+        return error
+    if(',' not in regex):
+        isvalidregexfieldsingle(len(string.split(splitter)),regex,error)
+    else:
+        l = string.split(splitter)
+        maximum = len(l)
+        l = regex.split(",")
+        if(len(l)>maximum):
+            error.append("More items have been included than are present max accepted:"+str(maximum)+" your items:"+str(len(l)))
+            return error
+        for x in l:
+            r = isvalidregexfieldsingle(maximum,x,error)
+            if(len(r)>0):
+                return error
+    return error
 
 
 
 def usage():
-    print("\n\n-h --help\t\tshow help\n-f --file\t\tset url file\n-q --query\t\tset your custom keyword(Exaple: \"String1/String2/String3/String4_part1*String4_part2/Stri ng5\")\n-s --splitter\t\tset a splitter char (Example: splitter=\"/\" make --query=\"string1/string2\" two different keyword)\n-o --onlyscope\t\tdoesn't search external link found in url scope\n-t --tor\t\ttraffic over tor network(Make sure you have tor installed with port 9050 open)\n-p --path\t\tPath where you want save your result (Example: -p /var/www/html)\n\nUsage example:\tpython3 controller.py -f url.txt -q \"String1/String2/String3/String4_part1*String4_part2/Stri ng5\" -s \"/\" -o -p /var/www/html \r\n")
+    #print("\n\n-h --help\t\tshow help\n-f --file\t\tset url file\n-q --query\t\tset your custom keyword(Exaple: \"String1/String2/String3/String4_part1*String4_part2/Stri ng5\")\n-s --splitter\t\tset a splitter char (Example: splitter=\"/\" make --query=\"string1/string2\" two different keyword)\n-o --onlyscope\t\tdoesn't search external link found in url scope\n-t --tor\t\ttraffic over tor network(Make sure you have tor installed with port 9050 open)\n-p --path\t\tPath where you want save your result (Example: -p /var/www/html)\n\nUsage example:\tpython3 controller.py -f url.txt -q \"String1/String2/String3/String4_part1*String4_part2/Stri ng5\" -s \"/\" -o -p /var/www/html \r\n")
+    print("--generate-splitter\tGenerate a perfect splitter char \n\r\t\t\tUsage Example: --generate-splitter [YOUR_SEARCH_STRING_AND_REGEX] (concatenated without any splitter)\n\r\t\t\tExample of generation for words foo and foo2: --generate-splitter \"foofoo2\"")
+    print()
+    print("-h --help\t\tshow help")
+    print("-f --file\t\tset url file")
+    print("-q --query\t\tset your custom keyword(Exaple: \"String1/String2/String3/String4_part1*String4_part2/Stri ng5\")")
+    print("-s --splitter\t\tset a splitter char (Example: splitter=\"/\" make --query=\"string1/string2\" two different keyword)")
+    print("-o --onlyscope\t\tdoesn't search external link found in url scope")
+    print("-t --tor\t\ttraffic over tor network(Make sure you have tor installed with port 9050 open)")
+    print("-p --path\t\tPath where you want save your result (Example: -p /var/www/html)")
+    print("\n")
+    print("Usage example1:\t\tpython3 controller.py -f url.txt -q \"String1/String2/String3/String4_part1*String4_part2/Stri ng5\" -s \"/\" -o -p /var/www/html \r\n")
+    print("Usage example2:\t\tpython3 controller.py -f url.txt -q \"your_regex_1/String2/your_regex3/String4_part1*String4_part2/Stri ng5\" -s \"/\" -o -p /var/www/html -x 1,3")
+    print("WARNING:\nmake sure you don't have the char splitter in your regex if you use them")
 arr = ["-f or --file","-q or --query","-s or --splitter","-p or --path"]
 tor = False
 banner = open("banner.txt",'r')
 print(banner.read())
 banner.close()
 
+def generation(string):
+    string = string.strip()
+    if(len(string)==0):
+        return "The string cannot be empty..."
+    i = 0
+    while True:
+        r = ["~","!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","}","]","[","|","'",",",",",".",",","/","?",";",":","<",">"]
+        random.shuffle(r)
+        for x in r:
+            for j in range(i):
+                x+=random.choice(r)
+            if (x not in string):
+                return x
+        r=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        random.shuffle(r)
+        for x in r:
+            for j in range(i):
+                x+=random.choice(r)
+                if (x not in string):
+                    return x
+        i+=1
+        
 def validatepath(path):
     app = path
     if (os.path.isfile(path)):
@@ -63,23 +129,29 @@ def validatepath(path):
     return path
 
 
-
-
 try:
-    opts, args = getopt.getopt(sys.argv[1:],"htf:q:s:op:",["help","tor","file=","query=","splitter=","onlyscope","path="])
+    opts, args = getopt.getopt(sys.argv[1:],"htf:q:s:op:x:g:",["help","tor","file=","query=","splitter=","onlyscope","path=","regex=","generate-splitter="])
     if (not len(sys.argv[1:])):
         usage()
         exit()
     param = ""
+    regex="False"
+    keyword = ""
+    splitter = ""
     for o,a in opts:
         if (o in ('-f','--file')):
             param+=" -a file="+a
             arr.remove("-f or --file")
+        elif (o in ('--generate-splitter')):
+            print("You can use this as splitter ==> "+generation(a))
+            exit()
         elif (o in ('-q','--query')):
+            keyword = a
             a = a.replace('"','\\"')
             param+=" -a string=\""+a+"\""
             arr.remove("-q or --query")
         elif (o in ('-s','--splitter')):
+            splitter = a
             a = a.replace('"','\\"')
             param+=" -a splitchar=\""+a+"\""
             arr.remove("-s or --splitter")
@@ -90,6 +162,9 @@ try:
             exit()
         elif (o in ('-o','--onlyscope')):
             param+=" -a onlyscope=yes"
+        elif (o in ('-x','--regex')):
+            regex = a
+            param+=" -a regex=\""+regex+"\""
         elif (o in ("-p",'--path')):
             path = validatepath(a)
             arr.remove("-p or --path")
@@ -102,11 +177,28 @@ try:
             tor = True
 except Exception as err:
     print(err)
+
+
+error=[]
+
+error = isvalidregexfield(0,splitter,keyword,regex,error)
+
+if(len(error)>0 and not regex=="False"):
+    print("REGEX ERROR:")
+    print("="*50)
+    for x in error:
+        arr.append(x)
+    print("Use the parameter -x/--regex as follows:\nExample1:  -x 1,2,3\nExample2 --regex 1,2,3")
+    print("="*50)
+
+
 if(len(arr)>0):
-    print("For go ahead you must set the following parameters:\n")
+    print("To move forward, correct the following inconsistencies:\n")
     for x in arr:
-        print("==>",x)
+        print("[ERROR] ==> ",x)
     exit()
+
+
 if(tor):
     if(torEnabled()):
         command="torify scrapy crawl search"+param
