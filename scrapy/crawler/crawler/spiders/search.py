1a9f54bde578ea5d4e3a8d679051eb3946b74b40
intel-xeon <37773731+intel-xeon@users.noreply.github.com>
2022-07-04 12:33:44 +0200

    Update search.py

--- scrapy/crawler/crawler/spiders/search.py
+++ scrapy/crawler/crawler/spiders/search.py
@@ -9,6 +9,8 @@ import html
 import os
 import re
 import w3lib.html
+import json
+import ast
 
 def testmultiple(q,stringa):
     app = 0
@@ -23,7 +25,7 @@ def testmultiple(q,stringa):
 def writereport(lista,path):
     w = "{\"data\":["
     for x in lista:
-        k = str(x).replace("'",'"')
+        k = json.dumps(x)
         w+=k
         w+=","
     w = w[0:len(w)-1]
@@ -54,8 +56,14 @@ def matchregex(string,response):
 def searchforstring(response,string,url,title,regex,index):
     element = ['a','abbr','acronym','address','applet','area','article','aside','audio','b','base','basefont','bdi','bdo','bgsound','big','blink','blockquote','body','br','button','canvas','caption','center','circle','cite','clipPath','code','col','colgroup','command','content','data','datalist','dd','defs','del','details','dfn','dialog','dir','div','dl','dt','element','ellipse','em','embed','fieldset','figcaption','figure','font','footer','foreignObject','form','frame','frameset','g','h1','h2','h3','h4','h5','h6','head','header','hgroup','hr','html','i','iframe','image','img','input','ins','isindex','kbd','keygen','label','legend','li','line','linearGradient','link','listing','main','map','mark','marquee','mask','math','menu','menuitem','meta','meter','multicol','nav','nextid','nobr','noembed','noframes','noscript','object','ol','optgroup','option','output','p','param','path','pattern','picture','plaintext','polygon','polyline','pre','progress','q','radialGradient','rb','rbc','rect','rp','rt','rtc','ruby','s','samp','script','section','select','shadow','slot','small','source','spacer','span','stop','strike','strong','style','sub','summary','sup','svg','table','tbody','td','template','text','textarea','tfoot','th','thead','time','title','tr','track','tspan','tt','u','ul','var','video','wbr','xmp']
     match = []
+    if(index in regex):
+        result = matchregex(string,response)
+        for x in result:
+            match.append(x)
+        match = list(set(match))
+        return {"url":url,"title":html.escape(title),"matched":match}
     for e in element:
-        try:            
+        try:
             for x in response.xpath('//'+e+'/text()').extract():
                 if (index not in regex):
                     if("*" in string):
@@ -68,11 +76,8 @@ def searchforstring(response,string,url,title,regex,index):
                             x = html.escape(x)
                             x = unicodedata.normalize("NFKD", x) # serve per pulire carattere sporco \xa0
                             match.append(x)
-                else:
+                #else:
                     #stringa = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
-                    result = matchregex(string,response)
-                    for x in result:
-                        match.append(x)
         except Exception:
             continue
         if (len(match)==0):
@@ -107,12 +112,39 @@ def extractLink(url,link):
     else:
         return uri
 
+def checkname(regex):
+    f = open("template.txt",'r')
+    r = ast.literal_eval(f.read())
+    f.close()
+    for x in r:
+        if(r[x]==regex):
+            return x
+    return regex
+    
+
+
 def existKey(key,list_json):
     for x in list_json:
         if(x["key"]==html.escape(key)):
             return True
     return False
     
+def readtemplate(template):
+     f = open("template.txt",'r')
+     r = ast.literal_eval(f.read())
+     f.close()
+     h = template.split(",")
+     i = 0
+     list_string = []
+     for x in r:
+         i+=1
+         if(str(i) in h):
+             list_string.append(r[x])
+     regex = ""
+     for x in range(len(list_string)):
+         x+=1
+         regex+=str(x)+","
+     return (list_string,regex[0:len(regex)-1])
 
 class searchSpider(scrapy.Spider):
     
@@ -129,15 +161,20 @@ class searchSpider(scrapy.Spider):
     list_json=[]
     
     def start_requests(self):
-        if (len(self.splitchar)==0):
-            print ("Sorry.. you must specify splitchar exit..")
-            time.sleep(3)
-            return
         try:
             self.regex = self.regex.split(",")
         except Exception:
             self.regex = [-1]
+        try:
+            self.splitchar
+        except Exception:
+            self.splitchar = -1
         urls=[]
+        list_string = []
+        try:
+            l_string,self.regex = readtemplate(self.template)
+        except Exception:
+            l_string = self.string.split(self.splitchar)
         r = open(self.file,'r')
         for x in r:
             if (not x.startswith("http")):
@@ -149,9 +186,9 @@ class searchSpider(scrapy.Spider):
         for url in urls:
             self.root = getHost(url)
             sw = True
-            yield scrapy.Request(url=url, callback=self.parse,cb_kwargs=dict(radix=self.root,switch=sw,list_json=self.list_json,regex=self.regex))
+            yield scrapy.Request(url=url, callback=self.parse,cb_kwargs=dict(radix=self.root,switch=sw,list_json=self.list_json,regex=self.regex,list_string=l_string))
 
-    def parse(self, response,radix,switch,list_json,regex):
+    def parse(self, response,radix,switch,list_json,regex,list_string):
         try:
             self.onlyscope
         except Exception:
@@ -186,18 +223,18 @@ class searchSpider(scrapy.Spider):
                     switch = True
                 else:
                     switch = False
-                yield scrapy.Request(url=proc, callback=self.parse,cb_kwargs=dict(radix=radix,switch=switch,list_json=list_json,regex=regex))
+                yield scrapy.Request(url=proc, callback=self.parse,cb_kwargs=dict(radix=radix,switch=switch,list_json=list_json,regex=regex,list_string=list_string))
         
-        list_string = self.string.split(self.splitchar)
         h = 0
         for x in list_string:
             h+=1
             if (len(x)==0):
                 continue
             matched = searchforstring(response,x,response.url,str(response.css('title::text').get()),regex,str(h))
+            x = html.escape(checkname(x))
             if len(matched["matched"])>0:
                 if (existKey(x,list_json)==False):
-                    list_json.append({'key':html.escape(x),'matched':[matched]})
+                    list_json.append({'key':x,'matched':[matched]})
                     writereport(list_json,self.path)
                 else:
                     i = 0
